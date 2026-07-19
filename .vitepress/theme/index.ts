8fb9c2cc9cb0344b8f62d39b6a59ec1adc16849d
nichuanfang <f18326186224@gmail.com>
2026-07-19 20:04:43 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,10 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
+import DefaultTheme from 'vitepress/theme-without-fonts'
 import OutlineExt from "./OutlineExt.vue";
 import './style.css'
+import "./custom.css";
 
 export default {
   extends: DefaultTheme,
