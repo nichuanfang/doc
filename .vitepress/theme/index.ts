6e46e9e728e7b6310f0f26cc872338b79a68db50
nichuanfang <f18326186224@gmail.com>
2026-07-27 14:10:18 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,6 +2,7 @@
 import { h } from "vue";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme-without-fonts";
+import OutlineScroll from "./components/OutlineScroll.vue";
 import "./style.css";
 
 export default {
@@ -9,6 +10,7 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      "aside-outline-before": () => h(OutlineScroll),
     });
   },
   enhanceApp({ app, router, siteData }) {
